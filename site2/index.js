18185629c48a042ca0cab7aab163a722d3d1761d
Vlad <15vlad_yanko08@bk.ru>
2025-02-05 13:32:10 +0300

    .

--- site2/index.js
+++ site2/index.js
@@ -1,7 +1,21 @@
-const videos = document.querySelectorAll("video")
+document.addEventListener("DOMContentLoaded", function () {
+    const video = document.getElementById("background-video");
+    const masks = document.querySelectorAll(".video-mask");
 
-videos.forEach(video => {
-    video.play()
-    video.currentTime = 0;
+    masks.forEach(mask => {
+        const x = mask.getAttribute("data-x");
+        const y = mask.getAttribute("data-y");
+
+       
+        const clonedVideo = video.cloneNode(true);
+        clonedVideo.style.position = "absolute";
+        clonedVideo.style.left = `-${x}px`;
+        clonedVideo.style.top = `-${y}px`;
+        clonedVideo.style.visibility = "visible"; 
+
+        mask.appendChild(clonedVideo);
+    });
+
     
+    video.play();
 });
